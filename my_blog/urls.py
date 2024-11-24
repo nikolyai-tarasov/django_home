d744ea75adf28946959a31fe1af41aa246b1d32b
nikolyai-tarasov <kolya.tarasov333@gmail.com>
2024-11-24 16:22:46 +0300

    final_1

--- my_blog/urls.py
+++ my_blog/urls.py
@@ -5,7 +5,7 @@ from my_blog.views import HomeView, PostDetailView, ReformPostViews
 app_name = MyBlogConfig.name
 
 urlpatterns = [
-    path('', HomeView.as_view(), name="home"),
+    path('home_blog/', HomeView.as_view(), name="home_blog"),
     path('post_detail/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
     path('reform_post/<int:pk>/', ReformPostViews.as_view(), name='reform_post'),
 
--- my_blog/views.py
+++ my_blog/views.py
@@ -8,19 +8,25 @@ class HomeView(ListView):
     model = Post
     template_name = 'home_blog.html'
     context_object_name = 'posts'
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.filter(publication=True)
+
 
 class PostDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
     success_url = reverse_lazy('my_blog: home_blog')
 
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        self.object.views_counter += 1
+        self.object.save()
+        return self.object
+
+
 class ReformPostViews(UpdateView):
     model = Post
     fields = ['heading', 'created_at', 'description', 'image']
     template_name = 'reform_post.html'
-    success_url = reverse_lazy('my_blog: home_blog')
-
-
-
-
-
+    success_url = reverse_lazy('my_blog:home_blog')
