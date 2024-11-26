7822f7e608f8b9b7496c2f671400c7e4d465472d
nikolyai-tarasov <kolya.tarasov333@gmail.com>
2024-11-26 17:15:55 +0300

    final_1

--- catalog/forms.py
+++ catalog/forms.py
@@ -10,9 +10,23 @@ class ProductForm(forms.ModelForm):
         model = Product
         fields = ['name','image','category','description','price','created_at',]
 
+    def __init__(self, *args, **kwargs):
+        super(ProductForm, self).__init__(*args, **kwargs)
+        self.fields['name'].widget.attrs.update({'class': 'form-control'})
+        self.fields['image'].widget.attrs.update({'class': 'form-control'})
+        self.fields['category'].widget.attrs.update({'class': 'form-control'})
+        self.fields['description'].widget.attrs.update({'class': 'form-control'})
+        self.fields['price'].widget.attrs.update({'class': 'form-control'})
+        self.fields['created_at'].widget.attrs.update({'class': 'form-control'})
+
     def clean_name(self):
         name = self.cleaned_data.get('name')
         if name.lower() in forbidden_words:
             raise ValidationError(f'В имени использовано запрещенное слово {name}')
         return name
+    def clean_price(self):
+        price = self.cleaned_data.get('price')
+        if float(price) < 0:
+            raise ValidationError('Цена продукта не может быть отрицательной')
+        return price
 
--- ап.py
+++
@@ -1,2 +0,0 @@
-sd = "DFSAS"
-print(sd.lower())
